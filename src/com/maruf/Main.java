f563bdd75303270586e0dc8f919d190fe58fa1a3
marufsiyam91 <marufsiyam9123@gmail.com>
2024-10-13 22:18:59 +0600

    first commit

---
+++ src/com/maruf/Main.java
@@ -0,0 +1,10 @@
+package com.maruf;
+
+public class Main {
+    public static void main(String[] args) {
+        String firstName = "Maruf Hossain";
+        String lastName = "Siyam";
+
+        System.out.println(firstName + ' ' + lastName);
+    }
+}
