a7417eabf4ccbcf6ccb101be34431fe5ee3cd3dd
marufsiyam91 <marufsiyam9123@gmail.com>
2024-10-13 22:41:15 +0600

    add variables and their output

--- src/com/maruf/Main.java
+++ src/com/maruf/Main.java
@@ -2,9 +2,24 @@ package com.maruf;
 
 public class Main {
     public static void main(String[] args) {
+        byte age = 25;
+        short year = 2028;
+        int reveniew = 25000;
+        float height = 5.6f;
+        char letter = 'm';
+        boolean trueOrFalse = true;
+
         String firstName = "Maruf Hossain";
         String lastName = "Siyam";
 
+
+
         System.out.println(firstName + ' ' + lastName);
+        System.out.println(age);
+        System.out.println(year);
+        System.out.println(reveniew);
+        System.out.println(height);
+        System.out.println(letter);
+        System.out.println(trueOrFalse);
     }
 }
