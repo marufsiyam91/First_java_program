15e38f5bd1ece143f4f58a098bb38ea167a48e55
marufsiyam91 <marufsiyam9123@gmail.com>
2024-10-14 12:28:32 +0600

    add input which takes input from user and print the result in the terminal

---
+++ src/com/maruf/Inputs.java
@@ -0,0 +1,11 @@
+package com.maruf;
+
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+                int rollNum = input.nextInt();
+                System.out.println("your roll number is " + rollNum);
+    }
+}
