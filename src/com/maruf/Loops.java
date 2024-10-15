8e2541c4a748439fe08a45bc124d248d078cd432
marufsiyam91 <marufsiyam9123@gmail.com>
2024-10-15 19:30:13 +0600

    add take a random number n and print until the number through for loop

--- src/com/maruf/Loops.java
+++ src/com/maruf/Loops.java
@@ -1,18 +1,34 @@
 package com.maruf;
 
+import java.util.Scanner;
+
 public class Loops {
     public static void main(String[] args){
 
-        int value = 1;
+//        int value = 1;
+//
+//        while(value != 11){
+//
+//            int innerValue = 1;
+//            while (innerValue != 11) {
+//                System.out.println(value + " * " + innerValue + " = " + (value * innerValue));
+//                innerValue++;
+//            }
+//            value++;
+//        };
 
-        while(value != 11){
 
-            int innerValue = 1;
-            while (innerValue != 11) {
-                System.out.println(value + " * " + innerValue + " = " + (value * innerValue));
-                innerValue++;
-            }
-            value++;
-        };
+        // print numbers from 1 to n
+
+
+        Scanner input = new Scanner(System.in);
+
+        int number = input.nextInt();
+
+        for(int num = 0; num <= number; num++){
+            System.out.println("the value is now " + num);
+
+            num++;
+        }
     }
 }
