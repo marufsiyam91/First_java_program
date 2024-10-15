be18a4c60a35cee2ad5a73eb65e3d15d1fbfd41f
marufsiyam91 <marufsiyam9123@gmail.com>
2024-10-15 18:28:37 +0600

    add take any name in input and print your name is the name

--- src/com/maruf/Inputs.java
+++ src/com/maruf/Inputs.java
@@ -5,8 +5,26 @@ import java.util.Scanner;
 public class Inputs {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("please enter your rollNumber here: ");
-        int rollNumber = input.nextInt();
-        System.out.println("your roll number is " + rollNumber);
+//        System.out.println("please enter your rollNumber here: ");
+//        int rollNumber = input.nextInt();
+//        System.out.println("your roll number is " + rollNumber);
+//
+//
+//
+//        String firstName = "Maruf Hossain";
+//        String lastName = "Siyam";
+//
+//        System.out.println(firstName + " " + lastName);
+
+//        int age = input.nextInt();
+//        System.out.println(age);
+//
+//        float year = input.nextInt();
+//        System.out.println();
+
+        System.out.println("give your name here: ");
+        String name = input.nextLine();
+        System.out.println("your name is " + name);
+
     }
 }
