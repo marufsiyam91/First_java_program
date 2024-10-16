bbe78efc830ef618f3668d1ae5055687aef30933
marufsiyam91 <marufsiyam9123@gmail.com>
2024-10-16 22:58:17 +0600

    add the code for finding the maximum number from the given numbers

--- src/com/maruf/FindMax.java
+++ src/com/maruf/FindMax.java
@@ -1,4 +1,24 @@
 package com.maruf;
 
+import java.util.Scanner;
+
 public class FindMax {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+        int max = a;
+
+        if(max < b){
+            max = b;
+        }
+        if(max < c){
+            max = c;
+        }
+
+        System.out.println(max);
+    }
 }
