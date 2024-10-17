6ebbc3882aaac816dd5657b4738a936c1b82dc57
marufsiyam91 <marufsiyam9123@gmail.com>
2024-10-17 21:57:18 +0600

    add switch case simplification with single line statement

--- src/com/maruf/SwitchCase.java
+++ src/com/maruf/SwitchCase.java
@@ -9,22 +9,12 @@ public class SwitchCase {
         String Language = input.nextLine();
 
         switch (Language){
-            case "C":
-                System.out.println("You are a pro programmer and your language is a lower level language");
-                break;
-            case "C++":
-                System.out.println("You are likely a better programmer and your language is a mid level language");
-                break;
-            case "Python":
-                System.out.print("Your language is a higher level object oriented programming language.");
-                break;
-            case "Javascript":
-                System.out.println("You write code in world's most popular programming language");
-                break;
-            case "Java":
-                System.out.println("Now you are coding on this language. and you are the programmer");
-            default:
-                System.out.println("In which language do you code and program");
+            case "C" -> System.out.println("You are a pro programmer and your language is a lower level language");
+            case "C++" -> System.out.println("You are likely a better programmer and your language is a mid level language");
+            case "Python" -> System.out.print("Your language is a higher level object oriented programming language.");
+            case "Javascript" -> System.out.println("You write code in world's most popular programming language");
+            case "Java" ->  System.out.println("Now you are coding on this language. and you are the programmer");
+            default ->  System.out.println("In which language do you code and program");
         }
     }
 }
