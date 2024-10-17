f838158941b972d9620e6c3b0c60c120fcd46e78
marufsiyam91 <marufsiyam9123@gmail.com>
2024-10-17 21:43:06 +0600

    add switch case program

--- src/com/maruf/SwitchCase.java
+++ src/com/maruf/SwitchCase.java
@@ -1,4 +1,30 @@
 package com.maruf;
 
+import java.util.Scanner;
+
 public class SwitchCase {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String Language = input.nextLine();
+
+        switch (Language){
+            case "C":
+                System.out.println("You are a pro programmer and your language is a lower level language");
+                break;
+            case "C++":
+                System.out.println("You are likely a better programmer and your language is a mid level language");
+                break;
+            case "Python":
+                System.out.print("Your language is a higher level object oriented programming language.");
+                break;
+            case "Javascript":
+                System.out.println("You write code in world's most popular programming language");
+                break;
+            case "Java":
+                System.out.println("Now you are coding on this language. and you are the programmer");
+            default:
+                System.out.println("In which language do you code and program");
+        }
+    }
 }
